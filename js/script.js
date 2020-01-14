e30826f79776c7d3786fbaccf49eb87bdd2e048b
hramn1 <stmihailov@yandex.ru>
2020-01-14 15:53:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ var swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true,
 });
+// Меню
+const dropDown = document.querySelectorAll('.dropdown-toggle');
+const dropMenu = document.querySelectorAll('.dropdown-menu');
+document.onclick = function f (evt) {
+  if (evt.target.classList.contains('dropdown-toggle') && !evt.target.nextElementSibling.classList.contains('dropdown-menu--open') ){
+    evt.preventDefault();
+    for (let i = 0; i < dropMenu.length; i++) {
+      if(dropMenu[i].classList.contains('dropdown-menu--open')){
+        dropMenu[i].classList.remove('dropdown-menu--open');
+      }
+    }
+    evt.target.nextElementSibling.classList.toggle('dropdown-menu--open');
+
+  } else {
+  for (let i = 0; i < dropMenu.length; i++) {
+        if(dropMenu[i].classList.contains('dropdown-menu--open')){
+          dropMenu[i].classList.remove('dropdown-menu--open')
+      }
+    }
+  }
+}
+for (let i = 0; i < dropDown.length; i++){
+  dropDown[i].onmouseleave = function g (evt) {
+  let timerId =  setTimeout(function () {
+      if(evt.target.nextElementSibling.classList.contains('dropdown-menu--open')){
+        evt.target.nextElementSibling.classList.remove('dropdown-menu--open');
+      }
+    }, 500)
+    evt.target.nextElementSibling.onmouseleave = () => {
+      let timerId1 = setTimeout(function () {
+       evt.target.nextElementSibling.classList.remove('dropdown-menu--open');
+     }, 500)
+     dropDown[i].onmouseenter = function () {
+       clearTimeout(timerId1);
+
+     }
+     evt.target.nextElementSibling.onmouseenter = function () {
+       clearTimeout(timerId1);
+     }
+    }
+
+    dropDown[i].onmouseenter = function () {
+      clearTimeout(timerId);
+
+    }
+    evt.target.nextElementSibling.onmouseenter = function () {
+      clearTimeout(timerId);
+    }
+  }
+}
