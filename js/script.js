9199648a6e72d0ad3d8b6da684f4acc64097887c
hramn1 <stmihailov@yandex.ru>
2020-01-14 14:38:56 +0300

    правки

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+var swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+});
