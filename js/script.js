69ecbf90a869d2418cf64a3c05619c0b7ec0b264
hramn1 <stmihailov@yandex.ru>
2020-01-17 17:30:51 +0300

    beta version 0.89

--- js/script.js
+++ js/script.js
@@ -14,6 +14,20 @@ var swiperPhoto = new Swiper('.swiper-container', {
 var swiperPrice = new Swiper('.price-service.swiper-container', {
   slidesPerView: 4,
   spaceBetween: 40,
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    1023: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1279: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  },
   freeMode: false,
   navigation: {
     nextEl: '.swiper-button-next',
@@ -70,3 +84,124 @@ for (let i = 0; i < dropDown.length; i++){
     }
   }
 }
+// Мобильное меню
+let mobilMenu = {
+  btnNav: document.querySelector('.btn--nav'),
+  menuList: document.querySelector('.main-menu'),
+  menuStartHeight: () => {
+    document.querySelector('.main-menu').style.visibility = 'hidden';
+
+    document.querySelector('.main-menu').style.display = 'flex';
+
+    document.querySelector('.main-menu').style.height = 'auto';
+    const height = document.querySelector('.main-menu').offsetHeight;
+    document.querySelector('.main-menu').style.display = 'none';
+    return height;
+  },
+  menuHeight: function () {
+    return mobilMenu.menuList.offsetHeight;
+  },
+  checkMenu: function () {
+    if (mobilMenu.btnNav.classList.contains('collapsed') && window.matchMedia('(max-width: 767px)').matches){
+      mobilMenu.menuList.style.display = "none";
+    }
+    else if (window.matchMedia('(max-width: 1025px)').matches) {
+      mobilMenu.menuList.style.display = "flex";
+    }
+    else {
+      mobilMenu.menuList.style.display = "flex";
+    }
+  },
+  showMenu: (mh) => {
+    mobilMenu.btnNav.classList.remove('collapsed');
+    mobilMenu.menuList.style.display = "flex";
+    document.querySelector('.main-menu').style.visibility = 'visible';
+    mobilMenu.menuList.style.height = 0;
+    let h = 0;
+    let animationMenu = setInterval(function () {
+      h+=30;
+      mobilMenu.menuList.style.height = h + 'px';
+      if (h >= mh) {
+        clearTimeout(animationMenu)
+      }
+    },30)
+  },
+  collapseMenu: () => {
+    mobilMenu.btnNav.classList.add('collapsed');
+    let mh =  mobilMenu.menuList.offsetHeight;
+    let animationMenu = setInterval(function(){
+      mh -=40;
+      mobilMenu.menuList.style.height = mh + 'px';
+      if (mh <= 0 ){
+        clearTimeout(animationMenu)
+        mobilMenu.menuList.style.display = 'none';
+
+      }
+    },30)
+  },
+  toggleMenu: () => {
+    if(mobilMenu.btnNav.classList.contains('collapsed')){
+      mobilMenu.showMenu(mobilMenu.menuStartHeight());
+      mobilMenu.btnNav.classList.add('btn--close');
+    } else {
+      mobilMenu.collapseMenu();
+    mobilMenu.btnNav.classList.remove('btn--close');
+    }
+  },
+}
+mobilMenu.checkMenu();
+window.addEventListener("resize", mobilMenu.checkMenu);
+mobilMenu.btnNav.addEventListener('click',mobilMenu.toggleMenu);
+
+// Аккордеон
+const btnAcc = document.querySelectorAll('.btn-colapse');
+const collapseBlock = document.querySelectorAll('.colapse');
+function accStartHeight (block) {
+  block.style.visibility = 'hidden';
+  block.style.display = 'block';
+  block.style.height = 'auto';
+  const height = block.offsetHeight;
+  block.style.display = 'none';
+  accShow(height, block);
+  //return height;
+}
+function accShow(mh, block) {
+  block.style.display = 'block';
+  block.style.visibility = 'visible';
+  block.style.height = 0;
+  let h = 0;
+  let animationMenu = setInterval(function () {
+    h+=30;
+    block.style.height = h + 'px';
+    if (h >= mh) {
+      block.style.height = mh + 'px';
+      clearTimeout(animationMenu)
+    }
+  },30)
+}
+function collapseAcc(block) {
+    let mh =  block.offsetHeight;
+    let animationMenu = setInterval(function(){
+      mh -=40;
+      block.style.height = mh + 'px';
+      if (mh <= 0 ){
+        clearTimeout(animationMenu)
+        block.style.display = 'none';
+      }
+    },30)
+  }
+for (let i = 0; i < btnAcc.length; i++) {
+    btnAcc[i].addEventListener('click',function(evt) {
+      btnAcc[i].classList.toggle('btn-colapse--show');
+
+      if(btnAcc[i].dataset.target == collapseBlock[i].id){
+        collapseBlock[i].classList.toggle('colapse--show');
+        if(btnAcc[i].classList.contains('btn-colapse--show')){
+          accStartHeight(collapseBlock[i]);
+        }
+        else {
+          collapseAcc(collapseBlock[i])
+        }
+      }
+    })
+  }
